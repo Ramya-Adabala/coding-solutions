fda28ac5ac44d9fba0034fecf4e9e755a0ec0aaf
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-08-13 22:26:57 +0530

    ✅ FYISUJ25 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-13

---
+++ codechef/medium/FYISUJ25/solution.java
@@ -0,0 +1,9 @@
+ class Codechef{
+    public static void main(String[] args){
+        // Cook your dish here 
+        long transactionID=5123456789000L;
+        short branchCode=3052;
+        System.out.println("Transaction ID: "+transactionID);
+        System.out.println("Branch Code: "+branchCode);
+    }
+}
