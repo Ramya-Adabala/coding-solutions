2ef27039c914607de51444013bd75168dff202b1
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-08-15 21:43:06 +0530

    ✅ FYISUJ30 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-15

---
+++ codechef/medium/FYISUJ30/solution.java
@@ -0,0 +1,14 @@
+class Codechef {
+    public static void main(String[] args) {
+        //  Declare and assign values
+  float fullEfficiency=18.5f;
+  int dist=150;
+  System.out.println("Fuel Efficiency: "+fullEfficiency);
+  System.out.println("Distance Covered: "+dist);
+
+
+        //  Print the values
+
+
+    }
+}
