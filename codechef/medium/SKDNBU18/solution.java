ecd12cfecae5513015874283e5d180bab3449033
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-08-18 18:40:17 +0530

    ✅ SKDNBU18 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-18

---
+++ codechef/medium/SKDNBU18/solution.java
@@ -0,0 +1,11 @@
+class Codechef {
+    public static void main(String[] args) {
+        int costPerItem = 15; // Cost of one item
+
+        // Calculating total cost using *= operator
+        costPerItem *= 4;
+
+        // Printing the total cost
+        System.out.println("Total Cost: " + costPerItem);
+    }
+}
