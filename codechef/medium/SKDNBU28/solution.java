5591e5e5b7d726ab5e28e00f0a2042d4c28e2b2a
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-08-18 18:52:10 +0530

    ✅ SKDNBU28 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-18

---
+++ codechef/medium/SKDNBU28/solution.java
@@ -0,0 +1,11 @@
+class Codechef {
+    public static void main(String[] args) {
+        int candies = 25; // Total number of candies
+
+        // Calculating remaining candies using %= operator
+        candies %= 4;
+
+        // Printing the remainder candies
+        System.out.println("Remaining Candies: " + candies);
+    }
+}
