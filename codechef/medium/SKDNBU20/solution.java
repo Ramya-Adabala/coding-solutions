f7a8f8107ca1e78840bf71759a5cf40573bab6a4
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-08-18 18:42:40 +0530

    ✅ SKDNBU20 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-18

---
+++ codechef/medium/SKDNBU20/solution.java
@@ -0,0 +1,8 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Cook your dish here
+        double salary=45000.75;
+        double bonusMultiplier=1.10;
+        System.out.println(salary*bonusMultiplier);
+    }
+}
