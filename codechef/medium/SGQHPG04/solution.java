456d3955fb60cd7df45de1b98fa958fd9e0e72d6
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-08-21 16:12:17 +0530

    ✅ SGQHPG04 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-21

---
+++ codechef/medium/SGQHPG04/solution.java
@@ -0,0 +1,10 @@
+class Codechef {
+    public static void main(String[] args) {
+        int buttonClicks = 0; // Initial click count
+
+        // Simulating button clicks using post-increment
+        System.out.println("Click number: " + buttonClicks++);
+        System.out.println("Click number: " + buttonClicks++);
+        System.out.println("Click number: " + buttonClicks++);
+    }
+}
