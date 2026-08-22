d638c2c7a41df3746e8263052ad473e6db98ddbd
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-08-22 18:02:04 +0530

    ✅ NBFAGD23 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-22

---
+++ codechef/medium/NBFAGD23/solution.java
@@ -0,0 +1,10 @@
+class Codechef {
+    public static void main(String[] args) {
+        int passingMarks = 40;  // Minimum marks required to pass
+        int studentMarks = 45;  // Marks obtained by the student
+
+        boolean hasPassed = studentMarks >= passingMarks; // Store result in variable
+
+        System.out.println("Has the student passed? " + hasPassed);
+    }
+}
