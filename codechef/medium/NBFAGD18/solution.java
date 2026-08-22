1f47a24a853bb5625ab6361df02047d8fad394a3
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-08-22 18:00:09 +0530

    ✅ NBFAGD18 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-22

---
+++ codechef/medium/NBFAGD18/solution.java
@@ -0,0 +1,8 @@
+class Codechef {
+    public static void main(String[] args) {
+        int minTemperature = 10;  // Minimum safe temperature
+        int currentTemperature = 5; // Current temperature
+
+        System.out.println("Is temperature too low? " + (currentTemperature < minTemperature));
+    }
+}
