c3b0b18e22d5f37db38be1681fe4207d91e2119c
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-08-22 17:57:56 +0530

    ✅ NBFAGD09 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-22

---
+++ codechef/medium/NBFAGD09/solution.java
@@ -0,0 +1,16 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Total working days in a month
+        int totalWorkingDays = 22;
+
+        // Days attended by an employee
+        int daysAttended = 20;
+
+        // Complete the comparision
+        boolean isAttendanceIncomplete = true;
+
+        // Printing the result directly
+        System.out.println("Attendance incomplete: " + isAttendanceIncomplete);
+
+    }
+}
