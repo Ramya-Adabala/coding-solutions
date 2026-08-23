007ed843ea01df939607e7120d04a6e56ae8fc2d
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-08-23 17:35:56 +0530

    ✅ LASTLEVELS - Rating 679 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-23

---
+++ codechef/easy/LASTLEVELS---Rating-679/solution.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int x=sc.nextInt();
+		    int y=sc.nextInt();
+		    int z=sc.nextInt();
+		    int tot=x*y;
+		    int breaks=(x-1)/3;
+		    tot+=breaks*z;
+		    System.out.println(tot);
+		}
+
+	}
+}
