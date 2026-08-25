2058a5ba80a4a6a5f3bce6a983fde9facc39a971
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-08-25 19:39:40 +0530

    ✅ QUOMQD05 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-25

---
+++ codechef/medium/QUOMQD05/solution.java
@@ -0,0 +1,10 @@
+class Codechef {
+    public static void main(String[] args) {
+        
+        String key = "SecurePass";
+        // write your code here
+        System.out.println("The length of the string is "+key.length());
+      
+        
+    }
+}
