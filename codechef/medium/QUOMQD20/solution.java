0082cfd7d477db9bd714ddf3cd4877779ce32426
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-08-25 20:20:05 +0530

    ✅ QUOMQD20 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-25

---
+++ codechef/medium/QUOMQD20/solution.java
@@ -0,0 +1,13 @@
+class Codechef {
+    public static void main(String[] args) {
+        String filePath = "/home/user/documents/file.txt";
+
+        // Extract the file name (substring from index 21 to 29)
+        String fileName = filePath.substring(21,29);
+
+        // Print the extracted file name
+        System.out.println("File Name: " + fileName);
+
+    }
+}
+
