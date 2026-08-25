a0fcf4621105230db49f5a8b3d6cc40eca4d44fc
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-08-25 19:40:20 +0530

    ✅ QUOMQD13 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-25

---
+++ codechef/medium/QUOMQD13/solution.java
@@ -0,0 +1,14 @@
+class Codechef {
+    public static void main(String[] args) {
+        
+        
+        String name = "Mr. John Smith";
+
+        // The substring method extracts the part of the string starting from index 4 (after "Mr. ")
+        String cleanName = name.substring(4);
+
+        
+        // This will display "John Smith"
+        System.out.println("Name without prefix: " + cleanName);
+    }
+}
