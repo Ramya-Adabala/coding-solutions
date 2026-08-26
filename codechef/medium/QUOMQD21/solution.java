a3dff73835c00f23b02717434ec49344d174114c
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-08-26 09:21:47 +0530

    ✅ QUOMQD21 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-26

---
+++ codechef/medium/QUOMQD21/solution.java
@@ -0,0 +1,9 @@
+class Codechef {
+    public static void main(String[] args) {
+        
+        // write your code here
+        String productDescription = "Laptop HP Pavilion";
+        String brand=productDescription.substring(7,9);
+        System.out.println(brand);
+    }
+}
