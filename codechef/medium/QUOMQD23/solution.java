329c0067720158b5d3599c6356c370dbda4fd467
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-08-26 09:20:14 +0530

    ✅ QUOMQD23 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-26

---
+++ codechef/medium/QUOMQD23/solution.java
@@ -0,0 +1,12 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Define a string
+        String word = "programming";
+
+        // Find the index of the first occurrence of the vowel 'o'
+        int indexOfO = word.indexOf('o');
+
+        // Print the result
+        System.out.println("The index of 'o' is: " + indexOfO);
+    }
+}
