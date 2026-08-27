4bcae0d164f634a23a9700b56deb2cffb2512e3c
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-08-27 07:30:11 +0530

    ✅ NDLQGP06 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-27

---
+++ codechef/medium/NDLQGP06/solution.java
@@ -0,0 +1,22 @@
+class Codechef {
+    public static void main(String[] args) {
+        
+        String s1="Hello";
+        String s2="World";
+        
+        // Converting both strings to uppercase
+         s1= s1.toUpperCase();
+         s2= s2.toUpperCase();
+
+
+        // Concatenating with a space in between
+         String s3=s1 + " "  +s2;
+
+        // Printing the length of the final string
+              System.out.println(s3.length());
+
+        // Printing the final string
+       System.out.println(s3);
+        
+    }
+}
