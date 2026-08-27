740c53389fcb35bab3e73e473636f8e24c93c41c
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-08-27 07:34:30 +0530

    ✅ NDLQGP11 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-27

---
+++ codechef/medium/NDLQGP11/solution.java
@@ -0,0 +1,13 @@
+class Codechef {
+    public static void main(String[] args) {
+        
+        // Define a variable productName and its value 
+        String productName = "Samsung GALAXY Phone";
+        
+        // Use toLowerCase() method to print the product name in lowercase for storage
+        productName=productName.toLowerCase();
+        
+        // Print the desired results
+        System.out.println("Formatted Product Name: "+productName);
+    }
+}
