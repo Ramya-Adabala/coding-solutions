36653ca62cbad145080c637a2db6f571c65b1687
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-08-27 07:38:35 +0530

    ✅ NDLQGP19 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-27

---
+++ codechef/medium/NDLQGP19/solution.java
@@ -0,0 +1,13 @@
+class Codechef {
+    public static void main(String[] args) {
+        
+        // Define a variable named subjectName and provide it a value.
+        String subjectName = "Mathematics@Physics@Chemistry";
+        
+        // Use replace() method to replace '@' with '_'
+        String modified=subjectName.replace('@','_');
+
+        // Print the desired results
+        System.out.println("Modified subject name: "+ modified);
+    }
+}
