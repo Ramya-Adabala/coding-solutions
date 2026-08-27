de95ebd6f5f96aa79fb2567a56e0a855d917e16e
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-08-27 07:44:20 +0530

    ✅ NDLQGP25 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-27

---
+++ codechef/medium/NDLQGP25/solution.java
@@ -0,0 +1,10 @@
+class Codechef {
+    public static void main(String[] args) {
+        
+        String description = "This shirt is available in multiple colours.";
+        description=description.replace("colours","color");
+       System.out.println(description);
+        
+
+    }
+}
