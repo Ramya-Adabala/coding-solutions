34d3002d78333114e14574568f314a24ed4d8227
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-08-29 21:46:24 +0530

    ✅ WNDHVZ16 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-29

---
+++ codechef/medium/WNDHVZ16/solution.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Codechef {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Taking user input for total books and number of students
+        int totalBooks = scanner.nextInt();
+        int students = scanner.nextInt();
+
+        // Calculating books per student and remaining books
+  int booksPerStudent=totalBooks/students;
+  int remainingBooks=totalBooks%students;
+
+
+        // Printing the output based on the condition
+
+       if (remainingBooks == 0) {
+            System.out.println("Each student gets " + booksPerStudent + " books equally.");
+        } else {
+            System.out.println("Each student gets " + booksPerStudent + " books, but " + remainingBooks + " books remain undistributed.");
+        }
+
+
+
+    }
+}
