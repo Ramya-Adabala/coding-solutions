931654fb04b63f8c41ef6c1fb4390d0d249fb53e
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-09-01 07:55:10 +0530

    ✅ RESUFF18 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-01

---
+++ codechef/medium/RESUFF18/solution.java
@@ -0,0 +1,13 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Initialize the variable 'age' with a predefined value (20)
+        int age = 20; 
+
+        
+        String eligibility = (age >= 18) ? "Eligible to vote" : "Not eligible to vote";
+
+        // Output the eligibility result to the console
+        System.out.println(eligibility);
+    }
+}
+
