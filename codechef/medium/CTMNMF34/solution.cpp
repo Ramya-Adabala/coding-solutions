f12fb3a57a2c2f92055dd6779e2531f467c5738b
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-09-03 06:45:23 +0530

    ✅ CTMNMF34 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-03

---
+++ codechef/medium/CTMNMF34/solution.cpp
@@ -0,0 +1,15 @@
+class Codechef {
+    public static void main(String[] args) {
+     
+       // This infinite loop simulates a continuously running server.
+        while(true)
+        {
+             System.out.println("Server is running");
+             //Write break; below
+             
+             break;
+             
+        }
+    
+    }
+}
