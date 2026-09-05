76491bd7403d4b62d287525d5fe9ef19311e0a77
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-09-05 15:42:56 +0530

    ✅ PSPP115 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-05

--- codechef/medium/PSPP115/solution.py
+++ codechef/medium/PSPP115/solution.py
@@ -3,9 +3,9 @@
 student_info = {"name": "Alice", "age": 25, "course": "Python"}
 
 # Adding a new key-value pair
-student_info["location"]="New York";
+student_info["location"]="New York"
 
 # Modifying an existing value
-student_info["age"]=26;
+student_info["age"]=26
 
 print("Updated Student Info:", student_info)
