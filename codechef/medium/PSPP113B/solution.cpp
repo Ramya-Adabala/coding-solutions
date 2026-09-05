35b6dd307154422e756e052123093bd0a8c58e24
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-09-05 15:39:29 +0530

    ✅ PSPP113B | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-05

---
+++ codechef/medium/PSPP113B/solution.cpp
@@ -0,0 +1,10 @@
+student = (["Alice", "Bob"], ["Math", "Science", "English"], 20, 'XYZ')
+
+# Update the code below this line
+list_1, list_2, num, string = student
+
+# print the relevant components
+print(list_1[1])
+print(list_2[2])
+print(num)
+print(string[2])
