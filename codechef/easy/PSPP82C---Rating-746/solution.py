b3183af5333ec2995ceab2791435c8edc24412f6
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-09-05 15:35:17 +0530

    ✅ PSPP82C - Rating 746 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-09-05

---
+++ codechef/easy/PSPP82C---Rating-746/solution.py
@@ -0,0 +1,18 @@
+# Update the code below to solve the problem
+
+def square(t):
+    value1=t*t
+    return value1
+
+
+    
+def half(i):
+    value2=i//2
+    return value2
+ 
+ 
+ 
+n = int(input())
+result1 = square(n)
+result2 = half(result1)
+print(result2)
