f3acdd4ca6e2692ef34a6b5a1e4e456417f3d2e1
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-09-06 10:37:38 +0530

    ✅ GSQ53 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-09-06

---
+++ codechef/medium/GSQ53/solution.sql
@@ -0,0 +1,26 @@
+/* Write a query which does the following */
+
+-- Create a table 'customer'
+
+Create table customer(
+id int,
+name text,
+age int,
+address text 
+);
+
+
+
+
+
+-- Insert data into the table 
+insert into customer(id, name,age,address)
+
+VALUES(1, 'John Smith', 25,  '123 Main St'),
+(2, 'Sarah Johnson', 30,'456 Broadway'),
+(3, 'Michael Brown', 45, '789 5th Ave'),
+(4, 'Jessica Davis', 28, '321 Elm St'),
+(5, 'David Lee', 35,  '555 Park Ave');
+
+-- Output the entire table
+select *from customer;
