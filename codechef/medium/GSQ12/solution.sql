c5b8742f982e867db5ab2a812c8ebb1e3d896596
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-09-06 10:27:39 +0530

    ✅ GSQ12 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-09-06

--- codechef/medium/GSQ12/solution.sql
+++ codechef/medium/GSQ12/solution.sql
@@ -1,6 +1,5 @@
-/* Write a query to do the following
-- Set hourly_pay to 150 for HR employees
-- Output the entire table
-*/
-update employee set hourly_pay=150 where department='Hr';
+/* Write a query which does the following
+- Delete all rows in the employee table whose Department is 'Hr'.
+- Output all the entires of the table. */
+delete from employee where Department='Hr';
 select *from employee;
