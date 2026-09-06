d4fdbd1ea4f3480b09cc096eb981708df3bb967a
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-09-06 10:46:49 +0530

    ✅ PSPP118 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-06

---
+++ codechef/medium/PSPP118/solution.py
@@ -0,0 +1,6 @@
+# Debug the code below to solve the problem
+
+student_info = {"name": "Alice", "age": 25, "course": "Python"}
+
+for info in student_info:
+    print("Key:", info, ", Value:", student_info[info])
