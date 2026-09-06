d321f2010a7d613d88fda3e832a37011ce056bcd
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-09-06 11:00:53 +0530

    ✅ Queue Traversal | Basic | Java | Runtime: N/A | Memory: N/A | 2026-09-06

---
+++ gfg/basic/queue-traversal-1594111322/solution.java
@@ -0,0 +1,8 @@
+class Solution {
+    public ArrayList<Integer> queueTraversal(Queue<Integer> q) {
+        // code here
+        ArrayList<Integer> al=new ArrayList<>();
+        for(int n:q) al.add(n);
+        return al;
+    }
+}
