ea31cc08695b241c36de3612250e3fab8cd22ebe
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-09-06 10:46:47 +0530

    ✅ PSPP117 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-06

--- codechef/medium/PSPP117/solution.py
+++ codechef/medium/PSPP117/solution.py
@@ -1,14 +1,6 @@
+# Debug the code below to solve the problem
+
 student_info = {"name": "Alice", "age": 25, "course": "Python"}
 
-# Using del to remove a specific key-value pair
-del student_info["course"]
-print(student_info)
-
-# Using pop() to remove and retrieve a specific key-value pair
-removed_age = student_info.pop("age")
-
-# Using clear() to clear all key-value pairs
-student_info.clear()
-
-print("Updated Student Info:", student_info)
-print("Removed Age:", removed_age)
+for info in student_info:
+    print("Key:", info, ", Value:", student_info[info])
