f6af17e5539decc2ac8658e0dbbb5affd021245f
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-09-06 10:30:15 +0530

    ✅ GSQ13 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-09-06

--- codechef/medium/GSQ13/solution.sql
+++ codechef/medium/GSQ13/solution.sql
@@ -1,5 +1,10 @@
-/* Write a query which does the following
-- Delete all rows in the employee table whose Department is 'Hr'.
-- Output all the entires of the table. */
-delete from employee where Department='Hr';
-select *from employee;
+/* Write a query to create a table employee with the mentioned constraints on the columns : 
+employee_id - PRIMARY KEY, 
+employee_Name -UNIQUE, 
+department -NOT NULL CHECK (department IN ('Sales', 'Finance', 'Operations')) */
+
+create table employee(
+employee_id integer primary key,
+employee_Name text UNIQUE,
+department text not null check(department in ('Sales','Finance','Operations'))
+);
