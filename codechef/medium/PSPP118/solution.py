d5fc32e3fef1c6df52955e96bed173b022472502
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-09-06 10:47:07 +0530

    ✅ PSPP118 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-06

--- codechef/medium/PSPP118/solution.py
+++ codechef/medium/PSPP118/solution.py
@@ -1,6 +1,11 @@
-# Debug the code below to solve the problem
+# Click on submit to view the result
 
 student_info = {"name": "Alice", "age": 25, "course": "Python"}
 
-for info in student_info:
-    print("Key:", info, ", Value:", student_info[info])
+# Using the 'in' operator to check for key existence
+print("name" in student_info)  
+print("location" in student_info)  
+
+# Using the 'get()' method to check for key existence
+print(student_info.get("age"))  
+print(student_info.get("location"))  
