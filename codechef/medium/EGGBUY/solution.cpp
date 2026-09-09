9575d3e4e5ea4a476ebe4ada8c8968c0ddb22989
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-09-09 20:04:02 +0530

    ✅ EGGBUY | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-09

---
+++ codechef/medium/EGGBUY/solution.cpp
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		int y=sc.nextInt();
+		int f=sc.nextInt();
+		int first=x*12;
+		int second=y*12;
+		System.out.println(Math.min(first,(second+3)));
+
+	}
+}
