c3f4e83adffb93170b1eeffcdc691fbdd20cc3a3
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-09-11 11:45:46 +0530

    ✅ GSQ45 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-09-11

---
+++ codechef/medium/GSQ45/solution.sql
@@ -0,0 +1,4 @@
+ /* Write a query to find the Average of the column 'Payout' across 'Department' from the table 'employee'. The output table should have the average payout of each department.*/
+ select department, avg(payout) as avg_payout from employee 
+ group by department;
+
