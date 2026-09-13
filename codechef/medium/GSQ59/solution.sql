ac6dba2fd0f470f652e77c8ff712f00e1c275159
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-09-13 16:29:47 +0530

    ✅ GSQ59 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-09-13

--- codechef/medium/GSQ59/solution.sql
+++ codechef/medium/GSQ59/solution.sql
@@ -1,5 +1,5 @@
-/* Write a query to output the following
-- Department and average payout on a single line
-- Where total payout of the department is more than 40. */
-select department, avg(payout) as avg_payout from employee
-group by department having sum(payout) >40;
+/* Debug the below query */
+
+SELECT department, Avg(Age) as 'avg_age'
+FROM employee group by Department
+HAVING Count(Department) > 3;
