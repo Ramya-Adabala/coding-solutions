9071a19ea12ac333a6c1c4cd09f94447f4377a5b
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-09-17 19:48:19 +0530

    ✅ GS14 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-17

--- codechef/medium/GS14/solution.py
+++ codechef/medium/GS14/solution.py
@@ -1,6 +1,11 @@
-t = int(input())
-for i in range(t):
-    # take input and output the join using +
-    s=input()
-    x=s+s
-    print(x)
+# Solution as follows
+
+# accept the count of test cases given in the the 1st line
+t = int(input())        
+
+# run a loop to accept 't' inputs
+for i in range(t):      
+    n = int(input())    
+    
+    # output the negative integer - i.e. (-n)
+    print(-n)
