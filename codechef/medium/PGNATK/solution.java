692ae48ba1d77908c4624b708690eb8073fee411
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-09-23 22:54:06 +0530

    ✅ PGNATK | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-23

--- codechef/medium/PGNATK/solution.java
+++ codechef/medium/PGNATK/solution.java
@@ -7,6 +7,14 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int n=sc.nextInt();
+		    int k=sc.nextInt();
+		    int delay=(n-1)/(k-1);
+		    System.out.println(n+delay);
+		}
 
 	}
 }
