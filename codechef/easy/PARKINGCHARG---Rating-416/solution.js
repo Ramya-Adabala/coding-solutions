1815d4c4e77958358e2fb72228c9770594df97f4
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-09-24 17:16:26 +0530

    ✅ PARKINGCHARG - Rating 416 | Easy | JavaScript | Runtime: N/A | Memory: N/A | 2026-09-24

---
+++ codechef/easy/PARKINGCHARG---Rating-416/solution.js
@@ -0,0 +1,15 @@
+// Complete the code 
+
+function totalAmount(x, y, h){
+    console.log(x+(h-1)*y);
+}
+
+// Input related code. Please do not change this.
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', function(input) {
+  const nums = input.trim().split(' ');
+  const x = parseInt(nums[0]); 
+  const y = parseInt(nums[1]); 
+  const h = parseInt(nums[2]); 
+  totalAmount(x, y, h);
+});
