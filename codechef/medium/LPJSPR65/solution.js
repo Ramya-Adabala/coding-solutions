9e826e2410b3fef448174d927fe5cbd1e3c54faf
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-09-25 14:03:51 +0530

    ✅ LPJSPR65 | Medium | JavaScript | Runtime: N/A | Memory: N/A | 2026-09-25

---
+++ codechef/medium/LPJSPR65/solution.js
@@ -0,0 +1,13 @@
+let name_1 = "Ajay";
+let name_2 = "Sumit";
+
+let marks_1 = 98.4;
+let marks_2 = 98.4;
+
+if (marks_2 < marks_1) {
+    console.log(name_2);
+} else if (marks_2 > marks_1) {
+    console.log(name_1);
+} else {
+    console.log("equal");
+}
