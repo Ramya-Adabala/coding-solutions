4f46605e1055fdf1ebcc92722db276b94e6535af
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-09-26 11:25:04 +0530

    ✅ QUEUE08 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-26

---
+++ codechef/medium/QUEUE08/solution.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int n=sc.nextInt();
+		    int k=sc.nextInt();
+		    int a[]=new int[n];
+		    for(int i=0;i<n;i++){
+		        a[i]=sc.nextInt();
+		    } 
+		   ArrayList<Integer> al1=new ArrayList<>();
+		   ArrayList<Integer> al2=new ArrayList<>();
+		   for(int i=k;i<n;i++){
+		       al1.add(a[i]);
+		   }
+		   for(int i=0;i<k;i++){
+		       al2.add(a[i]);
+		   }
+		   al1.addAll(al2);
+		   for(int i=0;i<n;i++){
+		       System.out.print(al1.get(i)+" ");
+		   }
+		   System.out.println();
+		  
+	}}
+}
